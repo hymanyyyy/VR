7e24bae7a4dc8d715f583f4d0133b3027614f484
hymanyyyy <hyman0924@gmail.com>
2024-04-11 11:34:36 +0800

    fjfjdxfj

---
+++ sea/data.js
@@ -0,0 +1,274 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "海洋廣場",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 1.6936642331213845,
+        "pitch": -0.0694836756953432,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.579949682352714,
+          "pitch": 0.056104351653786466,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": -2.1139475167696844,
+          "pitch": 0.11883916294010533,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": -0.16079897146778777,
+          "pitch": -0.9967849191774558,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.989318762002199,
+          "pitch": -0.17386596278710265,
+          "title": "文化中心",
+          "text": "基隆最大的圖書館，也是各項文藝活動的表演地方"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "海洋廣場東側",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5450134329935494,
+          "pitch": 0.2137179246598908,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 0.035847836298907865,
+          "pitch": -0.8699595909436013,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.0557583076933135,
+          "pitch": -0.011291650738185055,
+          "title": "好食城",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "海洋廣場西側",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9781644643274134,
+          "pitch": 0.34093097505268943,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 1.5915655033623342,
+          "pitch": 0.08356832350661136,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": 2.4182307920333557,
+          "pitch": -0.5156965864088807,
+          "rotation": 5.497787143782138,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.657076967885903,
+          "pitch": -0.18200451568799458,
+          "title": "基隆火車站",
+          "text": "近幾年才新蓋，可說是基隆最漂亮的建築，也是上班族，和遊客的必經地"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "西岸",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.105378895079065,
+          "pitch": 0.153222775064199,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": 0.47270474182006694,
+          "pitch": -1.0279988453228075,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.4572369229631974,
+          "pitch": -0.4098942184581027,
+          "title": "陽明海運",
+          "text": "<div>修繕並命名為「陽明海洋文化藝術館」，期望透過各種文化、藝術、教育、特展及活動等多元方式，永續推動海洋文化教育工作。</div><div><br></div>"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "海洋廣場環景",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1330622091258196,
+          "pitch": 1.3077254679941674,
+          "rotation": 3.141592653589793,
+          "target": "0-"
+        },
+        {
+          "yaw": 1.3834934284747433,
+          "pitch": 0.905425494438612,
+          "rotation": 8.63937979737193,
+          "target": "1-"
+        },
+        {
+          "yaw": -1.3318891518166538,
+          "pitch": 0.6064180617797348,
+          "rotation": 3.9269908169872414,
+          "target": "2-"
+        },
+        {
+          "yaw": -0.7941225002674237,
+          "pitch": 0.48272058996473,
+          "rotation": 5.497787143782138,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.096632530215146,
+          "pitch": 0.180448261876208,
+          "title": "皇冠大樓",
+          "text": "33樓基隆最高大樓，內涵電影院，各大知名餐廳，各項遊樂設施等，是基隆納涼的好地方"
+        }
+      ]
+    }
+  ],
+  "name": "海洋廣場",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
