c597beb56a53f77e192f1278e5576b15fc7c5de0
hymanyyyy <hyman0924@gmail.com>
2024-04-11 11:53:43 +0800

    wtrjht

---
+++ park/data.js
@@ -0,0 +1,280 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "潮境公園",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.203539192857045,
+          "pitch": 0.1632062014355391,
+          "rotation": 5.497787143782138,
+          "target": "1-a"
+        },
+        {
+          "yaw": -2.5458513111666043,
+          "pitch": -0.6416936091716536,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": 0.1989363053922375,
+          "pitch": -0.08482696345830121,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.3696604105753334,
+          "pitch": 0.07956002955190655,
+          "title": "潮境公園",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-a",
+      "name": "潮境公園A",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.21121612399080725,
+          "pitch": 0.2244578110284028,
+          "rotation": 0.7853981633974483,
+          "target": "0-"
+        },
+        {
+          "yaw": 3.059217859370481,
+          "pitch": 0.041765921431940356,
+          "rotation": 5.497787143782138,
+          "target": "2-b"
+        },
+        {
+          "yaw": -2.388498523761008,
+          "pitch": -0.7315321912387933,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.208733722539842,
+          "pitch": 0.08398377820421565,
+          "title": "夜衝活動",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2-b",
+      "name": "潮境公園B",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0520641596664433,
+          "pitch": 0.16427400716880314,
+          "rotation": 0,
+          "target": "1-a"
+        },
+        {
+          "yaw": 1.8856580779090013,
+          "pitch": -0.9694460590456373,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.4591361140727503,
+          "pitch": -0.032078929935885014,
+          "title": "基隆嶼",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "飛天掃把",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.163913908022466,
+        "pitch": 0.3251349949633777,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.0540979954822216,
+          "pitch": 0.42792417205555466,
+          "rotation": 5.497787143782138,
+          "target": "0-"
+        },
+        {
+          "yaw": -1.1158243852290468,
+          "pitch": -0.8411400169160963,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.4502596680726292,
+          "pitch": 0.12407962059107724,
+          "title": "飛天掃把",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "潮境公園環景",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.7938382893085265,
+          "pitch": 0.11094072362405605,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 2.3167643158593467,
+          "pitch": 0.08527608954919685,
+          "rotation": 11.780972450961727,
+          "target": "1-a"
+        },
+        {
+          "yaw": 1.6528803806486527,
+          "pitch": 0.04026856817087143,
+          "rotation": 5.497787143782138,
+          "target": "2-b"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.05875328599845986,
+          "pitch": 0.037993763474247544,
+          "title": "山海關",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "潮境公園",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
